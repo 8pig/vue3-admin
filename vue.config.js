914461031996bdff24969d800f0a6de1c15e524c
zhaozhou <zhou773@foxmail.com>
2022-05-09 01:23:11 +0800

    test: 测试gzip 插件

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,9 @@
 const path = require('path');
-
+const IS_PROD = ['production', 'prod', 'test'].includes(process.env.NODE_ENV);
+const IS_DEV = ['development'].includes(process.env.NODE_ENV);
+// gzip
+const CompressionWebpackPlugin = require('compression-webpack-plugin');
+const productionGzipExtensions = /\.(js|css|json|txt|html|ico|svg)(\?.*)?$/i;
 function resolve (dir) {
   return path.join(__dirname, dir);
 }
@@ -12,6 +16,60 @@ module.exports = {
         changeOrigin: true,
         pathRewrite: { '^/mock': '' }
       }
+    }
+  },
+  configureWebpack: config => {
+    const plugins = [];
+    if (IS_PROD) {
+      plugins.push(
+        new CompressionWebpackPlugin({
+          filename: '[path].gz[query]',
+          algorithm: 'gzip',
+          test: productionGzipExtensions,
+          threshold: 10240,
+          minRatio: 0.8
+        })
+      );
+      config.optimization = {
+        splitChunks: {
+          cacheGroups: {
+            common: {
+              name: 'chunk-common',
+              chunks: 'initial',
+              minChunks: 2,
+              maxInitialRequests: 5,
+              minSize: 0,
+              priority: 1,
+              reuseExistingChunk: true,
+              enforce: true
+            },
+            vendors: {
+              name: 'chunk-vendors',
+              test: /[\\/]node_modules[\\/]/,
+              chunks: 'initial',
+              priority: 2,
+              reuseExistingChunk: true,
+              enforce: true
+            },
+            elementUI: {
+              name: 'chunk-elementui',
+              test: /[\\/]node_modules[\\/]element-ui[\\/]/,
+              chunks: 'all',
+              priority: 3,
+              reuseExistingChunk: true,
+              enforce: true
+            },
+            echarts: {
+              name: 'chunk-echarts',
+              test: /[\\/]node_modules[\\/](vue-)?echarts[\\/]/,
+              chunks: 'all',
+              priority: 4,
+              reuseExistingChunk: true,
+              enforce: true
+            }
+          }
+        }
+      };
     }
   },
   chainWebpack (config) {
